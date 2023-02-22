4bb039caa29e9139cf1b4873f8dbbb0a24a93d81
Maha Kazmi <maaryammmm@gmail.com>
2023-02-22 23:59:15 +0500

    Designed half Sign-In page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,31 @@
 import React from 'react'
+
 import { useLocation, useNavigate } from 'react-router-dom'
 export default function Header() {
-    const location= useLocation();
-    const navigate= useNavigate();
-    function pathMathRoute(route)
-    {
-        if(route===location.pathname)
-        {
+    const location = useLocation();
+    function pathMathRoute(route) {
+        if (route === location.pathname) {
             return true
         }
     }
+    const navigate = useNavigate();
 
-  return (
-    <div className='bg-white border-b-2 shadow-sm sticky top-0 z-50'>
-      <header className= "flex px-3 py-3 justify-between items-center mx-auto max-w-6xl">
-        <div>
-            <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
-             alt="logo" className='h-6 cursor-pointer' 
-             onClick={()=>navigate("/")} />
+    return (
+        <div className='bg-white border-b-2 shadow-sm sticky top-0 z-50'>
+            <header className="flex px-3 py-3 justify-between items-center mx-auto max-w-6xl">
+                <div>
+                    <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+                        alt="logo" className='h-6 cursor-pointer'
+                        onClick={() => navigate("/")} />
+                </div>
+                <div>
+                    <ul className='flex space-x-10'>
+                        <li className={`cursor-pointer font-semibold ${pathMathRoute("/") && "text-black border-b-4 border-b-red-500"}`} onClick={() => navigate("/")}>Home</li>
+                        <li className={`cursor-pointer font-semibold ${pathMathRoute("/offers") && "text-black border-b-4 border-b-red-500"}`} onClick={() => navigate("/offers")}>Offers</li>
+                        <li className={`cursor-pointer font-semibold ${pathMathRoute("/sign-in") && "text-black border-b-4 border-b-red-500"}`} onClick={() => navigate("/sign-in")}>Sign In</li>
+                    </ul>
+                </div>
+            </header>
         </div>
-        <div>
-            <ul className='flex space-x-10'>
-                <li className= {`cursor-pointer font-semibold border-b-4 border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500"}`} onClick={()=>navigate("/")}>Home</li>
-                <li className= {`cursor-pointer font-semibold border-b-4 border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500"}`} onClick={()=>navigate("/offers")}>Offers</li>
-                <li className= {`cursor-pointer font-semibold border-b-4 border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`} onClick={()=>navigate("/sign-in")}>Sign In</li>
-            </ul>
-        </div>
-      </header>
-    </div>
-  )
+    )
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { useState } from "react"
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 
 export default function SignIn() {
+  // creating hook
+  const [showPassword, setShowPassword] = useState(false); //bec we don't wanna show pass in the beginning, as default
+  const [formData, setFormData] = useState(
+    {
+      email: "",
+      password: "",
+    });
+  const { email, password } = formData;
+  function onChange(e) {
+    setFormData((prevState) => ({ ...prevState, [e.target.id]: e.target.value }))
+  }
   return (
     <div>
-      Sign In
+      <section>
+        <h1 className='text-3xl my-3 text-center text-semibold'>Sign In</h1>
+      </section>
+      <div className='flex justify-center flex-wrap items-center px-6 py-12'>
+        <div className='mx-10 md:w-[70%] lg:w-[50%]'>
+          <img src="https://media.istockphoto.com/id/1368151370/photo/user-typing-login-and-password-cyber-security-concept.jpg?b=1&s=170667a&w=0&k=20&c=wm6YUMs03Bup4_9XcQaX61L711qJxAUExEJp_PWO8gI=" alt="signIn" className='rounded-xl w-full' />
+        </div>
+        <div className='w-full md:w-[70%] lg:w-[40%] lg:ml-15'>
+          <form action="">
+            <input className='w-full border-2 border-gray-500 px-2 py-2 bg-white text-xl rounded' type="email" placeholder='Email Address' name="" id="email" value={email} onChange={onChange} />
+            <div className='relative'>
+              <input className='w-full border-2 border-gray-500 px-2 py-2 my-8 bg-white text-xl rounded'
+                type={showPassword ? "text" : "password"}
+                placeholder='Password' name="" id="password" value={password} onChange={onChange} />
+              {showPassword ? (<AiFillEyeInvisible className="absolute right-3 cursor-pointer bottom-12"
+                onClick={() => setShowPassword((prevState) => !prevState)} />) : 
+                (<AiFillEye className="absolute right-3 cursor-pointer bottom-12" 
+                onClick={() => setShowPassword((prevState) => !prevState)} />)
+              }
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
